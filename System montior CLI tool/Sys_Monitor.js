1cd7c17513d9926f98c21c90fc64d940e1148d68
Tushar Sharma <onlyforcode23@gmail.com>
2025-10-08 18:26:46 +0100

    remove node modules folder and update main file

--- System montior CLI tool/Sys_Monitor.js
+++ System montior CLI tool/Sys_Monitor.js
@@ -1,16 +1,46 @@
 const si = require("systeminformation");
+
+
 const {Command} = require("commander");
+
+
 // We have used object destructuring here.we have extracted a class from commander module which is "Command" not command.we can give it a custom name too like command,mycommand,cmd etc.
+
+
 // const {Command:command} = require("commander");
+
+
+
+
 
 const program = new Command();
 
+
+
+
+
 program
+
+
 .name("System-Info")
+
+
 .description("Provides all the information related to system monitoring")
+
+
 .version("1.0.0");
 
+
+
+
+
 // program.option('--cpuInfo',"Provides the information related to CPU");
+
+
+
+
+
+
 
 
 // const Operations = program.opts();
@@ -18,48 +48,143 @@ program
 
 
 
+
+
+
+
+
+
+
+
+
+
 program.command('cpuInfo')
+
+
 .description("Provides CPU related information")
+
+
 .action(()=>{
+
+
     const cpuInfo = si.cpu();
+
+
     cpuInfo.then((data)=>{
+
+
         console.log(data);
+
+
     })
+
+
     .catch((err)=>{
+
+
         console.log(err);
+
+
     })
+
+
 })
+
+
+
+
+
+
 
 
 program.command('RAMInfo')
+
+
 .description("Provides real-time RAM related information")
+
+
 .action(()=>{
+
+
     const ramInfo = si.mem();
+
+
     ramInfo.then((data)=>{
+
+
         console.log(data);
+
+
     })
+
+
     .catch((err)=>{
+
+
         console.log(err);
+
+
     })
+
+
 })
+
+
+
+
 
 program.command('HdRAMInfo')
+
+
 .description("Provides Hardware-level RAM related information")
+
+
 .action(()=>{
+
+
     const hdRam = si.memLayout();
+
+
     hdRam.then((data)=>{
+
+
         console.log(data);
+
+
     })
+
+
     .catch((err)=>{
+
+
         console.log(err);
+
+
     })
+
+
 })
 
+
+
+
+
 si.cp
+
+
+
+
 
 program.parse()
 
 
 
-// console.log(sysInfo)
 
+
+
+
+
+
+
+
+// console.log(sysInfo)
