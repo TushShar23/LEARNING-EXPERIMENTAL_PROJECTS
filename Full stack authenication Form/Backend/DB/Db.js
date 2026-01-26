4e5360bc0aa112ddc619b6765b9e587ee93da317
Tushar Sharma <onlyforcode23@gmail.com>
2026-01-26 17:04:40 +0000

    backend routes complete

--- Full stack authenication Form/Backend/DB/Db.js
+++ Full stack authenication Form/Backend/DB/Db.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 import { Schema } from "mongoose";
 
 const UserSchema = new Schema({
-    firstname: {type:String,required:true},
-    lastname: {type:string,required:true},
-    email: {type:string,unique:true,required:true},// primary key kinda
-    password: {type:String,required:true},
-    phone: {type:String,required:true,unique:true}
+    Firstname: {type:String,required:true},
+    Lastname: {type:string,required:true},
+    Email: {type:string,unique:true,required:true},// primary key kinda
+    Password: {type:String,required:true},
+    Phone: {type:String,required:true,unique:true}
 })
 
 export const UserModel = mongoose.model('Users','UserSchema','Users');
