586e562752eec0fa51b32790c7d57d2d7282079c
Tushar Sharma <onlyforcode23@gmail.com>
2026-04-10 20:22:13 +0100

    complete standard(US) BMI_calculator logic and visual part design not done

--- BMI-CALCULATOR/Components/Form.jsx
+++ BMI-CALCULATOR/Components/Form.jsx
@@ -8,6 +8,13 @@ export function Form() {
   // so in form there will be a heading and after that there should be two buttons one is one for standard calculator and one for metric
   const [showform,setShowForm] = useState("Standard");
 
+  // i want to show the BMI state in the form so i lifted the state "BMI" and sent the setBmi updator function as prop to the component.
+
+  const [Bmi,setBmi] = useState({
+        BMI_Val:"",
+        BMI_msge:""
+    })
+
   function changeForm(e){
     setShowForm(e.target.id)
     // this e.target.id will give the name of the button clicked
@@ -15,14 +22,18 @@ export function Form() {
 
   return (
     <div className='container'>
-        <h1>BMI CALCULATOR</h1>
-        <div className="btns">
-          <button onClick={(e)=>changeForm(e)} id='Standard'>Standard</button>
-          <button onClick={(e)=>changeForm(e)} id='Metric'>Metric</button>
-        </div>
+      <h1>BMI CALCULATOR</h1>
+      <div className="btns">
+        <button onClick={(e)=>changeForm(e)} id='Standard'>Standard</button>
+        <button onClick={(e)=>changeForm(e)} id='Metric'>Metric</button>
+      </div>
 
-        {showform === "Standard" ? <Standard/> : <Metric/>}
+      {showform === "Standard" ? <Standard setBmi = {setBmi}/> : <Metric/>}
 
+      <div className="res">
+        <h3>{Bmi.BMI_Val}</h3>
+        <p>{Bmi.BMI_msge}</p>
+      </div>
     </div>
   )
 }
--- BMI-CALCULATOR/Components/Standard.jsx
+++ BMI-CALCULATOR/Components/Standard.jsx
@@ -1,5 +1,84 @@
-export function Standard(){
+import { useState } from "react"
+import "../src/Standard.css"
+
+
+export const Standard = ({setBmi})=>{
+    const [userData,setUserdata] = useState({
+        weight:"",
+        height:""
+    })
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+
+        const weight = parseFloat(userData.weight);
+        const height = parseFloat(userData.height);
+        
+        if(!weight || !height){
+            alert("Enter valid values");
+        }
+        else{
+            const result = 703 *( (weight) / (height * height));
+            const finalRes = Number(result.toFixed(2))
+
+            let message = "";
+            if(finalRes < 18.5){
+                message = "UnderWeight"
+            }
+            else if(finalRes < 25){
+                message = "Normal"
+            }
+            else if(finalRes < 30){
+                message = "OverWeight"
+            }
+            else{
+                message = "Obese"
+            }
+
+
+            setBmi({
+                BMI_Val:finalRes,
+                BMI_msge:message
+            })
+        }
+
+
+
+
+//         if (bmi < 18.5) category = "Underweight"
+// else if (bmi < 25) category = "Normal"
+// else if (bmi < 30) category = "Overweight"
+// else category = "Obese"
+    }
+
+    const handleInput = (e)=>{
+        const name = e.target.name
+        const value = e.target.value
+
+        setUserdata((prev)=>({...prev,[name]:value}))
+        // Implicitly returning object
+
+    }
+    //handleInput() will set the data inside userData state.
+
+
     return(
-       form
+       <form onSubmit={(e)=>handleSubmit(e)}>
+        <section className="weight">
+            <label htmlFor="Weight">
+                <strong>Weight(lbs)</strong>
+            </label>
+            <input type="text" placeholder="Enter your weight" id="Weight" name="weight" value={userData.weight} onChange={(e)=>handleInput(e)}/>
+        </section>
+
+        <section className="height">
+            <label htmlFor="Height">
+                <strong>Height(in)</strong>
+            </label>
+            <input type="text" placeholder="Enter your height" id="Height" name="height" value={userData.height} onChange={(e)=>handleInput(e)}/>
+        </section>
+
+        <button id="calculate" type="submit">CALCULATE</button>
+       </form>
     )
 }
